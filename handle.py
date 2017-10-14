9852f74d7668cf28a535a0c8021b1252413b6ac7
JinhaoPlus <rowin2013@gmail.com>
2017-10-14 13:37:37 +0800

    demo

--- handle.py
+++ handle.py
@@ -16,7 +16,7 @@ class Handle(object):
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                content = "test"
+                content = recMsg.Content + "真美"
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
                 return replyMsg.send()
             else:
